327ab5affa40fc08d2cab063c9ce924fed652596
IllyaKamen <60759046+IllyaKamen@users.noreply.github.com>
2023-01-09 14:48:36 +0200

    Add files via upload

---
+++ lab5/js/index.js
@@ -0,0 +1,128 @@
+function swapBlocks() {
+    const firstElement = document.getElementById("sidebar");
+    const sixthElement = document.getElementById("adv");
+
+    const blockOneContent = firstElement.textContent;
+
+    firstElement.textContent = sixthElement.textContent;
+    sixthElement.textContent = blockOneContent;
+
+}
+
+function countPentagonSquare() {
+    const firstElement = document.getElementById("fourth-text");
+
+    const side = 7;
+    const square = parseFloat(1/4 * (Math.sqrt(5*(5 + 2*Math.sqrt(5)))) * side * side).toFixed(1);
+
+    firstElement.textContent += "\n Pentagon square with side " + side + " is " + square;
+}
+
+function handleNumber(number) {
+    try {
+        const num = parseInt(number);
+        if (isNaN(num)) {
+            document.getElementById("error").textContent = "Please, input valid natural number";
+        } else {
+            document.getElementById("error").textContent = "";
+            const reverse = num.toString().split("").reverse().join('');
+            alert("The reverse number is " + reverse);
+            document.cookie = "reverseNumber=" + reverse;
+            document.cookie = "isOnLoad=true";
+        }
+    } catch (err) {
+        document.getElementById("error").textContent = "Please, input valid natural number";
+    }
+}
+
+
+function handleInstructions(instructions){
+    try {
+        if (instructions == null) {
+            document.getElementById("error1").textContent = "Please, input valid instructions";
+        } else {
+            document.getElementById("error1").textContent = "";
+            localStorage.setItem("addedStyles", instructions);
+            document.getElementById("content").style = localStorage.getItem("addedStyles");
+        }
+    } catch (err) {
+        document.getElementById("error1").textContent = "Please, input valid instructions";
+    }
+}
+
+function discardChanges(){
+    document.getElementById("content").removeAttribute('style')
+    localStorage.removeItem("addedStyles")
+}
+
+function displayForm(isDisplayed) {
+
+    const form = document.getElementById("formId");
+
+    if (isDisplayed) {
+        form.style = "display: block;";
+    } else {
+        form.style = "display: none;";
+    }
+}
+
+function deleteCookie(name) {
+    document.cookie = name + '=; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) { 
+        return parts.pop().split(';').shift() 
+    }
+}
+
+
+function selectEvent() {
+    const colorElement = document.getElementById("colorInput");
+    document.getElementById("sidebar").style = "background-color: " + colorElement.value + ";";
+    document.getElementById("brand-info").style = "background-color: " + colorElement.value + ";";
+    document.getElementById("title").style = "background-color: " + colorElement.value + ";";
+    document.getElementById("content").style = "background-color: " + colorElement.value + ";";
+    document.getElementById("links").style = "background-color: " + colorElement.value + ";";
+    document.getElementById("adv").style = "background-color: " + colorElement.value + ";";
+    document.getElementById("copyright").style = "background-color: " + colorElement.value + ";";
+    localStorage.setItem('allBoxColor', colorElement.value);
+
+}
+
+window.onload = (event) => {
+    let allBoxColor = localStorage.getItem("allBoxColor");
+    if (allBoxColor){
+        document.getElementById("sidebar").style = "background-color: " + allBoxColor + ";";
+        document.getElementById("brand-info").style = "background-color: " + allBoxColor + ";";
+        document.getElementById("title").style = "background-color: " + allBoxColor + ";";
+        document.getElementById("content").style = "background-color: " + allBoxColor + ";";
+        document.getElementById("links").style = "background-color: " + allBoxColor + ";";
+        document.getElementById("adv").style = "background-color: " + allBoxColor + ";";
+        document.getElementById("copyright").style = "background-color: " +allBoxColor + ";";
+    }
+
+    let temp = getCookie("reverseNumber");
+    if (temp) {
+        displayForm(false);
+        let hasToSave = confirm("Do you want to save the reversed number from cookie?: " + temp)
+        if (hasToSave) {
+            alert("Your cookies were saved. Page Reloaded")
+        } else {
+            temp = 0;
+            deleteCookie("reverseNumber");
+            deleteCookie("isOnLoad");
+        }
+    }
+
+    if (temp) {
+        alert("Current cookies are: reverseNumber=" + temp);
+    } else {
+        displayForm(true);
+        alert("Current cookies are:");
+    }
+}
+
+
